605c0ef2f7771e063d9d69bae1175d5b917e0009
groverkartik1998 <kartikgrover1998.kg@gmail.com>
2019-05-30 20:19:36 +0530

    Add projects file

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/' ,(req, res) => {
-  res.render('about.hbs', {
-    pageTitle : 'About page',
+  res.render('home.hbs', {
+    pageTitle : 'Home page',
+    welcomeMessage: 'Welcome to my website',
     currentYear : new Date().getFullYear()
   });
 });
 
 app.get('/about',(req, res) => {
   res.render('about.hbs', {
-    pageTitle : 'About page',
+    pageTitle : 'About Page',
+    currentYear : new Date().getFullYear()
+  });
+});
+
+app.get('/projects',(req, res) => {
+  res.render('projects.hbs', {
+    pageTitle : 'Projects',
     currentYear : new Date().getFullYear()
   });
 });
